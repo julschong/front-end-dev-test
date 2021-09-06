ae50e693adcef59460c609f142db30490cf5d5b3
Julius Lee <7julius.lee@gmail.com>
2021-09-06 03:05:29 -0700

    changed meta data

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,6 @@ import Banner1 from './components/Banner1';
 import Banner2 from './components/Banner2';
 import './App.scss';
 
-import bannerImage1 from './assets/banner-1.jpg';
-import bannerImage2 from './assets/banner-2.jpg';
 import Footer from './components/Footer';
 import Features from './components/Features/index';
 import Testimonial from './components/Testimonial/index';
