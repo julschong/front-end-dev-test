e46bc5665966bc4561d7f9b8fed33080c5afee67
Julius Lee <7julius.lee@gmail.com>
2021-09-09 00:42:47 -0700

    converted Testimonial and ReviewCarousel to use backend data

--- client/src/components/ReviewCarousel/index.js
+++ client/src/components/ReviewCarousel/index.js
@@ -1,8 +1,12 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { Carousel } from 'react-bootstrap';
 import './ReviewCarousel.scss';
+import { HomeContext } from './../../store/HomeContext';
 
 const ReviewCarousel = () => {
+    const { homeData } = useContext(HomeContext);
+    const { review } = homeData.data.testimonial;
+
     const [index, setIndex] = useState(0);
     const handleSelect = (selectedIndex, e) => {
         setIndex(selectedIndex);
@@ -14,48 +18,16 @@ const ReviewCarousel = () => {
             className="flex-column-center"
             variant="dark"
         >
-            <Carousel.Item>
-                <div>
-                    <h4>Joe Doe 08/08/2019</h4>
-                    <p className="detail-font">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Quod saepe modi, itaque accusantium, veritatis quibusdam
-                        earum eligendi distinctio quos, perferendis minus.
-                        Dolorem nulla aut rerum sequi, eum eius, minima incidunt
-                        error quae reiciendis dolor animi dolore harum? Ad
-                        tenetur, veritatis quaerat quasi illo voluptas atque
-                        architecto, quod illum explicabo ab.
-                    </p>
-                </div>
-            </Carousel.Item>
-            <Carousel.Item>
-                <div>
-                    <h4>Jane Doe 09/09/2020</h4>
-                    <p className="detail-font">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Quod saepe modi, itaque accusantium, veritatis quibusdam
-                        earum eligendi distinctio quos, perferendis minus.
-                        Dolorem nulla aut rerum sequi, eum eius, minima incidunt
-                        error quae reiciendis dolor animi dolore harum? Ad
-                        tenetur, veritatis quaerat quasi illo voluptas atque
-                        architecto, quod illum explicabo ab.
-                    </p>
-                </div>
-            </Carousel.Item>
-            <Carousel.Item>
-                <div>
-                    <h4>John Smith 01/09/2021</h4>
-                    <p className="detail-font">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Quod saepe modi, itaque accusantium, veritatis quibusdam
-                        earum eligendi distinctio quos, perferendis minus.
-                        Dolorem nulla aut rerum sequi, eum eius, minima incidunt
-                        error quae reiciendis dolor animi dolore harum? Ad
-                        tenetur, veritatis quaerat quasi illo voluptas atque
-                        architecto, quod illum explicabo ab.
-                    </p>
-                </div>
-            </Carousel.Item>
+            {review.map(({ author, createdDate, reviewContent }, i) => (
+                <Carousel.Item key={`${author + createdDate + i}`}>
+                    <div>
+                        <h4>
+                            {author} {createdDate}
+                        </h4>
+                        <p className="detail-font">{reviewContent}</p>
+                    </div>
+                </Carousel.Item>
+            ))}
         </Carousel>
     );
 };
--- client/src/components/Testimonial/index.js
+++ client/src/components/Testimonial/index.js
@@ -1,12 +1,17 @@
 import './Testimonial.scss';
 import { AiFillStar } from 'react-icons/ai';
 import ReviewCarousel from './../ReviewCarousel/index';
+import { useContext } from 'react';
+import { HomeContext } from './../../store/HomeContext';
 
 const Testimonial = () => {
+    const { homeData } = useContext(HomeContext);
+    const { title } = homeData.data.testimonial;
+
     return (
         <section id="testimonial" className="testimonial-container split ">
             <div className="test-left page-padding">
-                <h2 className="h2-font">LOREM IPSUM DOLOR SIT AMET</h2>
+                <h2 className="h2-font">{title}</h2>
                 <div className="star-group">
                     {[...Array(5)].map((el, i) => (
                         <AiFillStar size={27} fill="#FF7218" key={`star${i}`} />
