7bfc28cb90c064ac2deb4ba0f3f53551367c26c3
Julius Lee <7julius.lee@gmail.com>
2021-09-08 23:44:57 -0700

    converted banner1 to use backend

--- client/src/components/Banner1/index.js
+++ client/src/components/Banner1/index.js
@@ -1,17 +1,29 @@
 import './Banner1.scss';
+import { useContext } from 'react';
+import { HomeContext } from './../../store/HomeContext';
+import { BACKEND_URL } from '../../config/config';
 
 const BannerCTA = () => {
+    const { homeData } = useContext(HomeContext);
+    const { title, bannerImage, buttonText } = homeData.data.banner[0];
+
     return (
-        <section id="banner1" className="banner">
+        <section
+            id="banner1"
+            className="banner"
+            style={{
+                backgroundImage: `url("${BACKEND_URL}${bannerImage.url}"`
+            }}
+        >
             <div className="flex-column cta-container page-padding">
-                <h2 className="h2-font">LOREM IPSUM</h2>
+                <h2 className="h2-font">{title}</h2>
                 <button
                     className="custom-btn"
                     onClick={() => {
                         window.alert('Banner 1 button clicked');
                     }}
                 >
-                    BUTTON BUTTON
+                    {buttonText}
                 </button>
             </div>
         </section>
